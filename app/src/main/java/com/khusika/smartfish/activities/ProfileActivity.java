098e19cabc6e76acb453ba9db8243104f496244a
khusika <khusikadhamar@gmail.com>
2017-04-12 16:41:18 +0700

    Add FoldingCellLayout

---
+++ app/src/main/java/com/khusika/smartfish/activities/ProfileActivity.java
@@ -0,0 +1,92 @@
+package com.khusika.smartfish.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.khusika.smartfish.R;
+import com.khusika.smartfish.adapters.FoldingCellListAdapter;
+import com.khusika.smartfish.adapters.Item;
+import com.ramotion.foldingcell.FoldingCell;
+
+import java.util.ArrayList;
+
+/**
+ * Created by khusika on 12/04/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Intent i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        toolbar = (Toolbar) findViewById(R.id.profile_toolbar);
+        setupToolbar();
+
+        // get our list view
+        ListView theListView = (ListView) findViewById(R.id.mainListView);
+
+        // prepare elements to display
+        final ArrayList<Item> items = Item.getTestingList();
+
+        // add custom btn handler to first list item
+        items.get(0).setRequestBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "CUSTOM HANDLER FOR FIRST BUTTON", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // create custom adapter that holds elements and their state (we need hold a id's of unfolded elements for reusable elements)
+        final FoldingCellListAdapter adapter = new FoldingCellListAdapter(this, items);
+
+        // add default btn handler for each request btn on each item if custom handler not found
+        adapter.setDefaultRequestBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "DEFAULT HANDLER FOR ALL BUTTONS", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // set elements to adapter
+        theListView.setAdapter(adapter);
+
+        // set on click event listener to list view
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                // toggle clicked cell state
+                ((FoldingCell) view).toggle(false);
+                // register in adapter that state for selected cell is toggled
+                adapter.registerToggle(pos);
+            }
+        });
+
+    }
+
+    private void setupToolbar(){
+        toolbar.setTitle(getString(R.string.profile_title));
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() !=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = new Intent(ProfileActivity.this, MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+    }
+}
